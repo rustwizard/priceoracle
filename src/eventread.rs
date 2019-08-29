19a8fd4625cdc927d6bbbf9caa3fc1fc8219ed6d
Rust Wizard <rw@rustwizard.net>
2019-08-29 12:48:09 +0300

    add blocknum param to the config from which we start parsing ethereum logs

--- src/eventread.rs
+++ src/eventread.rs
@@ -5,10 +5,31 @@ use web3::types::{Address};
 #[folder = "src/contract/"]
 struct Asset;
 
+pub fn run_with_ws(logger: slog::Logger, arg: &ArgMatches) -> Result<(), String> {
+    let config = Config::new(arg);
+
+    info!(
+        logger,
+        "readevent runs on the {} network with contractaddr {}",
+        config.net,
+        config.contract_addr.unwrap(),
+    );
+
+    let (eloop, http) = web3::transports::WebSocket::new(&config.net).unwrap();
+    eloop.into_remote();
+
+    let _ = web3::Web3::new(http);
+
+
+
+    Ok(())
+}
+
 struct Config {
     contract_addr: Option<Address>,
     contract_abi: Vec<u8>,
     net: String,
+    block_num: i64,
 }
 
 impl Config {
@@ -27,6 +48,7 @@ impl Config {
             contract_addr: Some(contract_address),
             contract_abi,
             net,
+            block_num: 0
         }
     }
 }
