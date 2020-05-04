c070643f4248897b53f468955015bcf4a7c76150
Rust Wizard <rw@rustwizard.net>
2020-05-04 12:00:30 +0300

    issue#64 add hyper-tls for suppporting https requests

--- src/service.rs
+++ src/service.rs
@@ -1,13 +1,13 @@
 use clap::ArgMatches;
+use hyper::{Body, Client, Request};
+use hyper_tls::HttpsConnector;
 
 pub fn run(logger: slog::Logger, arg: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::new(arg);
-    info!(
-        logger,
-        "service called to the {}",
-        make_url(config.api_endpoint.unwrap(), config.api_key.unwrap()).unwrap()
-    );
-    Ok(())
+    let url = make_url(config.api_endpoint.unwrap(), config.api_key.unwrap()).unwrap();
+    info!(logger, "service called to the {}", url);
+
+    return send_request(url);
 }
 
 struct Config {
@@ -31,4 +31,14 @@ fn make_url(api_endpoint: String, api_key: String) -> Option<String> {
     let mut url = api_endpoint + "/data/price?fsym=BTC&tsyms=USD,ETH";
     url = url + "&api_key=" + api_key.as_ref();
     Some(url)
+}
+
+#[tokio::main]
+async fn send_request(url: String) -> Result<(), Box<dyn std::error::Error>> {
+    let req = Request::builder().uri(url).body(Body::empty()).unwrap();
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let resp = client.request(req).await?;
+    println!("Response: {}", resp.status());
+    Ok(())
 }
